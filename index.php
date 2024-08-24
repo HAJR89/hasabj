e9ccfc8192c60fbb43428b1d1dfac105c733bf30
abdu <abujamohammed@gmail.com>
2024-08-24 11:28:20 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+    <header>Daily Task</header>
+<head>
+    <title>Daily Task</title>
+</head>
+<body>
+    <h1>Manage your team & everything with Daily Task</h1>
+     <p>Unlock your potential, unleash your productivity, and experiece the joy of progress with our powerfull Daily Task.</p>
+     <a href="add_task.php">Let's Start</a> 
+</body>
+<footer>
+    <p>&copy; 2024 Daily Task</p>    
+</footer>
+</html>
